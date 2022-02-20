868aa0f7711df9b534657f93c350107010922b03
matosgabriel <gabriel.ifsal@gmail.com>
2022-02-19 23:33:49 -0300

    Implementing of authentication using NextAuth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -3,17 +3,21 @@ import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
 
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 export function SignInButton() {
-  const [isUserLogged, setIsUserLogged] = useState(true);
+  const { status, data } = useSession();
   
-  return isUserLogged ? (
-    <button type='button' className={styles.signInButton}>
+  console.log({ status, data });
+
+  return status === 'authenticated' ? (
+    <button type='button' className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub size={20} color="#04d361"/>
-      Gabriel Matos
+      { data.user.name }
       <FiX size={20} color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type='button' className={styles.signInButton}>
+    <button type='button' className={styles.signInButton} onClick={() => signIn('github')}>
       <FaGithub size={20} color="#eba417"/>
       Sign in with Github
     </button>
