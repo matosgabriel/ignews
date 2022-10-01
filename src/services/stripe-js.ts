8161051317762d78c9e5fe37c17d721af1fb885b
Gabriel <gabriel.ifsal@gmail.com>
2022-10-01 16:08:07 -0300

    Fixing the Stripe publishable key environment variable name

--- src/services/stripe-js.ts
+++ src/services/stripe-js.ts
@@ -11,7 +11,7 @@ import { Stripe, loadStripe } from "@stripe/stripe-js";
 let stripePromise: Promise<Stripe | null>;
 const getStripeJs = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
   }
   return stripePromise;
 };
