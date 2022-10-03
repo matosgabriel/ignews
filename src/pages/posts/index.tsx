6815ed0f5430c6aa006408b3c3c1156fb9981fa6
Gabriel <gabriel.ifsal@gmail.com>
2022-10-03 10:12:14 -0300

    Adding revalidate of 10 seconds on posts page static generation

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,17 +1,17 @@
-import Head from 'next/head';
-import styles from './styles.module.scss';
-import { GetStaticProps } from 'next';
-import { getPrismicClient } from '../../services/prismic';
-import Prismic from '@prismicio/client';
-import Link from 'next/link';
+import Head from "next/head";
+import styles from "./styles.module.scss";
+import { GetStaticProps } from "next";
+import { getPrismicClient } from "../../services/prismic";
+import Prismic from "@prismicio/client";
+import Link from "next/link";
 
 export interface PostFromPrismic {
   title: [{ text: string }];
-  content: [{ text: string, type: string }];
+  content: [{ text: string; type: string }];
 }
 
 interface Post {
-  slug: string,
+  slug: string;
   updatedAt: string;
   title: string;
   excerpt: string;
@@ -30,19 +30,17 @@ export default function Posts({ posts }: PostProps) {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          {
-            posts.map(post => {
-              return (
-                <Link href={`/posts/${post.slug}`} key={post.slug}>
-                  <a>
-                    <time>{post.updatedAt}</time>
-                    <strong>{post.title}</strong>
-                    <p>{post.excerpt}</p>
-                  </a>
-                </Link>
-              );
-            })
-          }
+          {posts.map((post) => {
+            return (
+              <Link href={`/posts/${post.slug}`} key={post.slug}>
+                <a>
+                  <time>{post.updatedAt}</time>
+                  <strong>{post.title}</strong>
+                  <p>{post.excerpt}</p>
+                </a>
+              </Link>
+            );
+          })}
         </div>
       </main>
     </>
@@ -53,25 +51,32 @@ export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
   const response = await prismic.query<PostFromPrismic>(
-    Prismic.Predicates.at('document.type', 'publication'), {
-      fetch: ['publication.title', 'publication.content'],
+    Prismic.Predicates.at("document.type", "publication"),
+    {
+      fetch: ["publication.title", "publication.content"],
     }
   );
 
-  const posts = response.results.map(post => ({
+  const posts = response.results.map((post) => ({
     slug: post.uid,
-    updatedAt: new Date(post.last_publication_date).toLocaleDateString('pr-BR', {
-      day: '2-digit',
-      month: 'long',
-      year: 'numeric'
-    }),
+    updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+      "pr-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
     title: post.data.title[0].text,
-    excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? ''
+    excerpt:
+      post.data.content.find((content) => content.type === "paragraph")?.text ??
+      "",
   }));
 
   // console.log(JSON.stringify(response, null, 2));
 
   return {
-    props: { posts }
+    props: { posts },
+    revalidate: 10, // 10 seconds
   };
-}
+};
